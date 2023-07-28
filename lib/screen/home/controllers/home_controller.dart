332bf32c5ff13fab022708ee050e041765ae89e1
indra <indraokisandy1@gmail.com>
2023-07-28 18:48:06 +0700

    add home

---
+++ lib/screen/home/controllers/home_controller.dart
@@ -0,0 +1,9 @@
+import 'package:get/get.dart';
+
+class HomeController extends GetxController {
+  // var storageService = Get.put(StorageService());
+  
+  // String? getToken() {
+  //   return storageService.read(GetStorageKey.token);
+  // }
+}
