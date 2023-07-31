051153b7cef50902476c3b5a41442436af7f1197
indra <indraokisandy1@gmail.com>
2023-07-31 09:28:42 +0700

    update show listproduct get api in view

--- lib/screen/home/controllers/home_controller.dart
+++ lib/screen/home/controllers/home_controller.dart
@@ -1,9 +1,13 @@
 import 'package:get/get.dart';
 
+import '../../../services/api_services/api_services.dart';
+
 class HomeController extends GetxController {
-  // var storageService = Get.put(StorageService());
-  
-  // String? getToken() {
-  //   return storageService.read(GetStorageKey.token);
-  // }
+  var listProductProvider = Get.put(ListProductProvider());
+
+  @override
+  void onInit() {
+    super.onInit();
+    listProductProvider.getProducts();
+  }
 }
--- lib/screen/home/views/home_view.dart
+++ lib/screen/home/views/home_view.dart
@@ -1,3 +1,4 @@
+import 'package:ecommerce_app/screen/home/widgets/custom_list_item.dart';
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
 
@@ -8,10 +9,29 @@ class HomeView extends GetView<HomeController> {
 
   @override
   Widget build(BuildContext context) {
-    return const Scaffold(
-      body: Center(
-        child: Text('Home View'),
-      ),
+    return Scaffold(
+      body: Obx(() {
+        if (controller.listProductProvider.products.isEmpty) {
+          // Show a loading indicator while data is being fetched.
+          return const Center(child: CircularProgressIndicator());
+        } else {
+          // Show the ListView with the fetched products.
+          return ListView.builder(
+            itemCount: controller.listProductProvider.products.length,
+            itemBuilder: (context, index) {
+              return Card(
+                  child: CustomListItem(
+                image: Image.network(
+                    controller.listProductProvider.products[index].image!),
+                title: controller.listProductProvider.products[index].title!,
+                description:
+                    controller.listProductProvider.products[index].description!,
+                price: controller.listProductProvider.products[index].price!,
+              ));
+            },
+          );
+        }
+      }),
     );
   }
 }
